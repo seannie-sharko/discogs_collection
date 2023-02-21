9373eb001ea60f073b9718cbc9d55442d94368a9
Sean <42978289+Sean-Brooks@users.noreply.github.com>
2023-02-20 20:14:16 -0500

    Update discogs_collection.py

--- discogs_collection.py
+++ discogs_collection.py
@@ -23,7 +23,7 @@ def get_user_auth():
 def get_record_collection(user):
 
     #
-    # Created a dict to allow more data control as the 
+    # Created a dict to allow more data control as the
     # discogs and rich Classes can be restrictive
     #
     record_collection = {}
@@ -31,35 +31,36 @@ def get_record_collection(user):
     table.add_column("Artist", justify="left", style="cyan")
     table.add_column("Album", justify="left", style="green")
     table.add_column("Format", justify="left", style="red")
-    for item in user.collection_folders[0].releases:
-        if item.release.artists[0].name in record_collection:
-            record_collection[item.release.artists[0].name].update({
-                item.release.title: ({
-                    'format': item.release.formats[0]['name'],
-                })
-            })
-        else:
-            record_collection.update({
-                item.release.artists[0].name: ({
+    with CONSOLE.status('Pulling your collection...', spinner='dots11'):
+        for item in user.collection_folders[0].releases:
+            if item.release.artists[0].name in record_collection:
+                record_collection[item.release.artists[0].name].update({
                     item.release.title: ({
                         'format': item.release.formats[0]['name'],
                     })
                 })
-            })
+            else:
+                record_collection.update({
+                    item.release.artists[0].name: ({
+                        item.release.title: ({
+                            'format': item.release.formats[0]['name'],
+                        })
+                    })
+                })
 
-    #
-    # Alphabetize Artists as keys
-    #
-    record_collection = OrderedDict(sorted(record_collection.items()))
+        #
+        # Alphabetize Artists as keys
+        #
+        record_collection = OrderedDict(sorted(record_collection.items()))
 
-    #
-    # Format data and add to table
-    #
-    for artist_name, album_values in record_collection.items():
-        for album_name, rformat in album_values.items():
-            table.add_row(artist_name,
-                          album_name,
-                          rformat['format'])
+        #
+        # Format data and add to table
+        #
+        for artist_name, album_values in record_collection.items():
+            for album_name, rformat in album_values.items():
+                table.add_row(artist_name,
+                              album_name,
+                              rformat['format'])
 
     CONSOLE.print(table)
     CONSOLE.print(user.collection_folders[0].count)
